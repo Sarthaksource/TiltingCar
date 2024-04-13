01fee9fa343e852189fe95c8b5f4c00dda1a612d
Sarthaksource <78723977+Sarthaksource@users.noreply.github.com>
2024-04-13 19:47:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,23 +8,36 @@ const road = new Road(carCanvas.width/2, carCanvas.width*0.9);
 // const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "TILT");
 
-const traffic = [
-	new Car(road.getLaneCenter(2), -100, 30, 50, "DUMMY", 2, getRandomColor()),
-	// new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2, getRandomColor()),
-	// new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2, getRandomColor()),
-	// new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2, getRandomColor()),
-	// new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2, getRandomColor()),
-	// new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2, getRandomColor()),
-	// new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2, getRandomColor()),
-	// new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2, getRandomColor()),
-	// new Car(road.getLaneCenter(1), -900, 30, 50, "DUMMY", 2, getRandomColor()),
-	// new Car(road.getLaneCenter(1), -900, 30, 50, "DUMMY", 2, getRandomColor()),
-];
+let yPosition = -100;
+
+
+const traffic=[];
+function addRandomTraffic(distance){
+	const cantCars=Math.floor(Math.random()*3);
+	for(let i=0;i<cantCars;i++){
+		const lane=Math.floor(Math.random()*3);
+		traffic.push(new Car(road.getLaneCenter(lane), -(car.y*-1+distance), 30, 50,"DUMMY",2,getRandomColor()));
+	}
+}
+addRandomTraffic(300);
+
+function deleteOldTraffic(){
+	for(let i=0;i<traffic.length;i++){
+		if(car.y < traffic[i].y-300){
+			traffic.splice(i,1);
+		}
+	}
+}
 
 animate();
 
 function animate(time)
 {
+	deleteOldTraffic();
+
+	if(parseInt(car.y)%(100*Car.getMaxSpeedValue()) == 0)
+		addRandomTraffic(800);
+
 	for(let i=0; i<traffic.length; i++)
 	{
 		traffic[i].update(road.borders, []);
